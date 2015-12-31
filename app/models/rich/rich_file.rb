7a376028be33d33029b5031e0d9eb8208ce415d0
Patrice Lebel <patrice.lebel@outlook.com>
2015-12-31 06:56:39 -0500

    use rich_file_file_name as name for RailsAdmin navigation

--- app/models/rich/rich_file.rb
+++ app/models/rich/rich_file.rb
@@ -6,6 +6,8 @@ module Rich
   class RichFile < ActiveRecord::Base
     include Backends::Paperclip
 
+    alias_attribute :name, :rich_file_file_name
+
     scope :images,  -> { where("rich_rich_files.simplified_type = 'image'") }
     scope :files,   -> { where("rich_rich_files.simplified_type = 'file'") }
 
